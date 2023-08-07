1f541d8dbffef5e53f98ad85dde96682524490c0
Maksim Samusin <maksim.samusin@rt.ru>
2023-08-07 13:20:42 +0300

    another one

---
+++ src/main/java/ru/sosiskabavarskaya/samplespringboot/SampleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ru.sosiskabavarskaya.samplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sosiskabavarskaya/samplespringboot/SampleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sosiskabavarskaya.samplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
